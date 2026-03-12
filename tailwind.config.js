7cfc8b3b4d94933b22efad6746f1ebdb51c2ec15
uolav <7643276+uolaV@users.noreply.github.com>
2026-03-12 10:12:44 +0000

    fix(css): add missing brand color shades + favicon (P0 followup)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
   content: ['./index.html', './app.js', './countries.js'],
   theme: {
     extend: {
-      fontFamily: { sans: ['Inter', 'sans-serif'] }
+      colors: {
+        brand: {
+          50:  '#f0f4ff',
+          100: '#e0eaff',
+          200: '#c7d2fe',
+          300: '#a5b4fc',
+          400: '#818cf8',
+          500: '#4f6ef7',
+          600: '#3a57e8',
+          700: '#2c43c7',
+          800: '#1e3499',
+          900: '#162370',
+        }
+      },
+      fontFamily: { sans: ['Inter', 'system-ui', 'sans-serif'] }
     }
   }
 }
